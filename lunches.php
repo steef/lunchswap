756e65856a44e271a2534e5a63d51c074db0bf2f
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2020-10-30 16:09:07 +0100

    ✨ Also save lunch at to database

--- lunches.php
+++ lunches.php
@@ -14,16 +14,34 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Get form info
+    // Get basic info
     $name = $_POST['name'];
-    $type = $_POST['type'];
     $allergy = $_POST['allergy'];
 
+    // Get lunch info
+    $lunches = [
+        'monday'    => $_POST['monday'],
+        'tuesday'   => $_POST['tuesday'],
+        'wednesday' => $_POST['wednesday'],
+        'thursday'  => $_POST['thursday'],
+        'friday'    => $_POST['friday']
+    ];
+
     // Save to database
-    $sql = "INSERT INTO lunches (name, type, allergy) VALUES (?,?,?)";
-    $stmt= $conn->prepare($sql);
-    $stmt->bind_param("sss", $name, $type, $allergy);
-    $stmt->execute();
+    foreach ($lunches as $day => $lunch) {
+        if ($lunch !== 'no_lunch') {
+            $sql = "INSERT INTO lunches (name, type, allergy, luch_at) VALUES (?,?,?,?)";
+            $stmt= $conn->prepare($sql);
+
+            $lunch_at = new DateTimeImmutable($day);
+            $result = $lunch_at->format('Y-m-d H:i:s');
+
+            $stmt->bind_param("ssss", $name, $lunch, $allergy, $result);
+            $stmt->execute();
+        }
+    }
+
+    // Close connection
     $conn->close();
 }
 
@@ -45,10 +63,47 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
             <label for="allergy">Allergy (optional) :</label>
             <input type="text" name="allergy" id="allergy">
         </p>
-        <h3>Monday</h3>
+        <h3>Lunches</h3>
         <p>
-            <label for="type">Type:</label>
-            <select type="text" name="type" id="type">
+            <label for="monday">Monday:</label>
+            <select type="text" name="monday" id="monday">
+                <option value="no_lunch">No lunch</option>
+                <option value="meat_fish">Meat/Fish</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="vegan">Vegan</option>
+            </select>
+        </p>
+        <p>
+            <label for="tuesday">Tuesday:</label>
+            <select type="text" name="tuesday" id="tuesday">
+                <option value="no_lunch">No lunch</option>
+                <option value="meat_fish">Meat/Fish</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="vegan">Vegan</option>
+            </select>
+        </p>
+        <p>
+            <label for="wednesday">Wednesday:</label>
+            <select type="text" name="wednesday" id="wednesday">
+                <option value="no_lunch">No lunch</option>
+                <option value="meat_fish">Meat/Fish</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="vegan">Vegan</option>
+            </select>
+        </p>
+        <p>
+            <label for="thursday">Thursday:</label>
+            <select type="text" name="thursday" id="thursday">
+                <option value="no_lunch">No lunch</option>
+                <option value="meat_fish">Meat/Fish</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="vegan">Vegan</option>
+            </select>
+        </p>
+        <p>
+            <label for="friday">Friday:</label>
+            <select type="text" name="friday" id="friday">
+                <option value="no_lunch">No lunch</option>
                 <option value="meat_fish">Meat/Fish</option>
                 <option value="vegetarian">Vegetarian</option>
                 <option value="vegan">Vegan</option>
