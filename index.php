824ecd99c91895279790b27faac7bedc8231c79c
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2020-10-30 10:47:58 +0100

    🎉 Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    	<title>LunchSwap 😋</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Welcome to LunchSwap!</p>";
+        ?>
+        <a href="lunches.php">Click here to add lunches 🍔</a> 
+    </body>
+</html>
---
+++ lunches.php
@@ -0,0 +1,97 @@
+<html>
+    <head>
+        <title>LunchSwap 😋</title>
+    </head>
+    <body>
+        <h2>Week 43 - Fill in your lunches 🍽</h2>
+        <a href="index.php">Click here to go back</a><br/><br/>
+        <h3>Monday</h3>
+        <form action="insert.php" method="post">
+            <p>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label for="type">Type:</label>
+                <select type="text" name="type" id="type">
+                    <option value="meat_fish">Meat/Fish</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+            </p>
+            <p>
+                <label for="allergy">Allergy:</label>
+                <input type="text" name="allergy" id="allergy">
+            </p>
+            <h3>Tuesday</h3>
+            <p>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label for="type">Type:</label>
+                <select type="text" name="type" id="type">
+                    <option value="meat_fish">Meat/Fish</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+            </p>
+            <p>
+                <label for="allergy">Allergy:</label>
+                <input type="text" name="allergy" id="allergy">
+            </p>
+            <h3>Wednesday</h3>
+            <p>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label for="type">Type:</label>
+                <select type="text" name="type" id="type">
+                    <option value="meat_fish">Meat/Fish</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+            </p>
+            <p>
+                <label for="allergy">Allergy:</label>
+                <input type="text" name="allergy" id="allergy">
+            </p>
+            <h3>Thursday</h3>
+            <p>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label for="type">Type:</label>
+                <select type="text" name="type" id="type">
+                    <option value="meat_fish">Meat/Fish</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+            </p>
+            <p>
+                <label for="allergy">Allergy:</label>
+                <input type="text" name="allergy" id="allergy">
+            </p>
+            <h3>Friday</h3>
+            <p>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label for="type">Type:</label>
+                <select type="text" name="type" id="type">
+                    <option value="meat_fish">Meat/Fish</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+            </p>
+            <p>
+                <label for="allergy">Allergy:</label>
+                <input type="text" name="allergy" id="allergy">
+            </p>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
